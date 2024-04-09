c8abb046180a7dee3d981f34d2d561c7398143e8
Daman2461 <142651012+Daman2461@users.noreply.github.com>
2024-04-09 22:40:49 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pickle
+from PIL import Image
+import sys
+
+
+from my_model import compress
+
+# Streamlit web app
+st.title("Image Compression App")
+
+# File uploader for image input
+uploaded_file = st.file_uploader("Upload a PNG Image", type=["png"])
+
+if uploaded_file is not None:
+    # Display the uploaded image
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+    
+    n=st.slider('Enter number of colors to compress into(number of centroids)',2,25)
+    it=st.slider('Enter number of iterations',1,15)
+    
+    
+    # Compress the uploaded image using the compress function
+    if st.button("Compress Image"):
+        compressed_image = compress(image,n,it)
+        if(compressed_image==-1).any():
+            st.error("Invalid Image")
+        
+        # Display the compressed image
+        else:
+            
+            st.image(compressed_image, caption="Compressed Image", use_column_width=True)
+        
+            st.success("Image compressed successfully.")
---
+++ my_model.py
@@ -0,0 +1,69 @@
+import numpy as np
+import random
+import pickle
+
+
+
+
+
+def closest(X,cen):
+    clo=np.zeros(X.shape[0], dtype=int)
+    for i in range(X.shape[0]):
+        m=[]
+        for j in range(cen.shape[0]):
+            m.append(np.sum((X[i]-cen[j])**2))
+        m=np.array(m)
+        clo[i]=m.argmin()
+    return clo
+
+
+def recompute_cen(x,cen,clo):
+    for i in range(cen.shape[0]):
+        p=x[i==clo]
+        if(p.shape[0]>0):
+            cen[i]=np.sum(p,axis=0)/p.shape[0]
+    return cen
+
+
+
+def compress(im,n=20,iterations=10):
+
+    a = np.asarray(im)
+    
+    
+    a=a/255.0
+    
+    original=a.shape
+    
+    if(a.shape[2]>3):
+        return -1
+    x  =np.reshape(a, (a.shape[0] * a.shape[1], 3))
+    
+    cen=[]
+    
+    
+    for i in range(n):
+        r=random.randint(0,x.shape[0])
+        cen.append(x[r])
+        
+    
+    cen=np.array(cen)
+    
+    
+    for i in range(iterations):
+        clo=closest(x,cen);
+        cen=recompute_cen(x, cen, clo)
+        print('Iteration '+str(i))
+    
+    x_compressed = cen[clo]
+    x_compressed = x_compressed.reshape( original)
+    
+   
+    
+    
+    x_compressed = (x_compressed * 255).astype(np.uint8)
+    
+   
+    return x_compressed
+
+
